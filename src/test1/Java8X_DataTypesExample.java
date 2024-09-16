19e648a11599fb47e5538984969b5ef952e9bc78
kumarvishnu759@gmail.com <kumarvishnu759@gmail.com>
2024-09-16 22:59:06 +0530

    commit added

---
+++ src/test1/Java8X_DataTypesExample.java
@@ -0,0 +1,24 @@
+package test1;
+
+public class Java8X_DataTypesExample {
+
+    public static void main(String[] args){
+
+        int myage=40;
+        boolean b=true;
+        float f=23.5f;
+        double d=67;
+        char ch='A';
+        String Myname="Welcome to India";
+        byte by=127;
+        Short s=768;
+
+
+        System.out.println(f);
+
+
+
+
+    }
+
+}
---
+++ src/test1/Java8x_JavaReverseString.java
@@ -0,0 +1,21 @@
+package test1;
+
+public class Java8x_JavaReverseString {
+
+    public static void main(String[] args){
+
+String s="India is beautiful country";
+String nstr="";
+char ch;
+
+for(int i=0; i<s.length();i++){
+    ch=s.charAt(i);
+    nstr=ch+nstr;
+
+}
+System.out.println("Reversed String is:" +nstr);
+
+    }
+
+
+}
