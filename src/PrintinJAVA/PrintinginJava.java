0860e64a56a586177d7df363fde6ca4f5cc9188b
kumarvishnu759@gmail.com <kumarvishnu759@gmail.com>
2024-09-17 09:47:20 +0530

    This is Print commit

---
+++ src/PrintinJAVA/PrintinginJava.java
@@ -0,0 +1,28 @@
+package PrintinJAVA;
+
+public class PrintinginJava {
+    public static void main(String[] args){
+
+        //Single line Printing
+
+        String Apple_Phone_16Series="iPhone16";
+        String Apple_iphone_15Series="iPhone15";
+
+        //Print here one by one
+System.out.println("Apple_Phone_16Series");
+System.out.println("Apple_iphone_15Series");
+
+//Print in single line
+       // System.out.print("Apple_Phone_16Series" );
+       // System.out.print( "Apple_iphone_15Series");
+
+        //Put space in between the two Strings
+        System.out.println(Apple_Phone_16Series+ " "+Apple_iphone_15Series);
+       // System.out.println("Apple_iphone_15Series");
+
+
+
+
+
+    }
+}
