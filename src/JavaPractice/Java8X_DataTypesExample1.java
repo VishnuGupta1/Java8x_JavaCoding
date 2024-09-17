4252b7580c80eac93b61428ad33d71e49e1c94ee
kumarvishnu759@gmail.com <kumarvishnu759@gmail.com>
2024-09-17 13:35:56 +0530

    Commit added by inidan time 1:35 PM

---
+++ src/JavaPractice/Java8X_DataTypesExample1.java
@@ -0,0 +1,50 @@
+package JavaPractice;
+
+public class Java8X_DataTypesExample1 {
+
+    public static void main(String[] args){
+        /*
+//Data Types
+        int myage=40;
+        boolean b=true;
+        float f=23.5f;
+        double d=67;
+        char ch='A';
+        String Myname="Welcome to India";
+        byte by=127;
+        Short s=768;
+
+
+        System.out.println(f);
+*/
+
+        /*
+
+        //PRINTF function
+// Problem statement - print the table of 2
+        // Formatting
+        // %d - any integer,byte, short, long
+        // %s - String
+        // %c - char
+        // %f -> float, double
+        int num = 100;
+       System.out.printf("Value of %d",num);//10
+        System.out.println("2x1=2");//2
+        System.out.printf("%d * %d = %d",num,1,num*1);//%d=10 and %d=3= 10*3
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,2,num*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,3,num*3);
+        */
+
+        //Example
+        String Firstname="Vishnu Kumar";
+        String lastname="gupta";
+
+System.out.printf("your name is firstname %s and lastname is %s", Firstname,lastname);
+
+
+
+    }
+
+}
