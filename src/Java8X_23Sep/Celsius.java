bfabc647d7637885a5cb71e3a9f4bbd4a6e4a41a
kumarvishnu759@gmail.com <kumarvishnu759@gmail.com>
2024-09-24 23:39:23 +0530

    Today Commited Second commmit added

--- src/Java8X_23Sep/Celsius.java
+++ src/Java8X_23Sep/Celsius.java
@@ -6,6 +6,13 @@ public class Celsius {
 
     public static void main(String[] args) {
 
+        /*
+        Write a program that converts between different units (e.g., kilometers to miles, Celsius to Fahrenheit) based on user selection using a switch statement.
+Input. -
+choice - 1 - km → m, km → 1km
+choice - 2 - f → c, f → c
+         */
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Select the conversion type:");
