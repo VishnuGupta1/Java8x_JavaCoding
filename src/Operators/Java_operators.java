35e26762228f3889b810681adce04f1799069128
kumarvishnu759@gmail.com <kumarvishnu759@gmail.com>
2024-10-18 09:11:05 +0530

    committ ID added

--- src/Operators/Java_operators.java
+++ src/Operators/Java_operators.java
@@ -1,6 +1,10 @@
 package Operators;
 
 public class Java_operators {
+    public static void main(String[] args) {
+
+
+
     /*
 
     ```
@@ -12,7 +16,50 @@ public class Java_operators {
 5. Ternary Operator ( variable = Expression1 ? Expression2: Expression3) -Expressions
  */
 
+        //1. Arithmetic operators (+ - ++ * / % -- )
+    int a = 5;
+    int b = 10;
+    /*
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);//division
+System.out.println(a%b); // remiander 2
+*/
+       //2. Assignment operators ( +=, =, -+, *=, /=, %=, &=, |=, >>=,<<=)
+        System.out.println(a+=b);//120
+        System.out.println(a=b); //100
+        System.out.println(a-+b);
+        System.out.println(a-+b);
+        System.out.println(a-+b);
+        System.out.println(a-+b);
+        /*
+//-= operator
+        int num1 = 40, num2 = 80;
 
+        System.out.println("num1 = " + num1);//10
+        System.out.println("num2 = " + num2);//20
 
+        // Subtracting & Assigning values
+        num1 -= num2;
+
+        // Displaying the assigned values
+        System.out.println("num1 = " + num1);
+*/
+        //( *= and %=) operator:
+
+        int num1 = 5, num2 = 3;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Multiplying & Assigning values
+        //num1 *= num2;
+        num1 %= num2;
+        // Displaying the assigned values
+        //System.out.println("num1 = " + num1);
+        System.out.println("num1 = " + num1);
+
+    }
 
 }
